4f7c226a2d2434232ba0c46e07ba12674db5cd74
Milena <142824252+Mel-b3002fA@users.noreply.github.com>
2025-02-16 20:09:53 +0100

    push

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+dataset = pd.read_csv('cancer.csv')
+x = dataset.drop(columns=["diagnosis(1=m, 0=b)"])
+y = dataset["diagnosis(1=m, 0=b)"]
+
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+import tensorflow as tf
+
+model = tf.keras.models.Sequential()
